e98d6e822aae693c67a32855d674e812682d2f56
Shashank Tripathi <113278561+tptbrs@users.noreply.github.com>
2025-11-14 14:05:05 +0530

    Update modules.js

--- client/src/modules.js
+++ client/src/modules.js
@@ -15,20 +15,20 @@ export default function lazyLoad(path, namedExport) {
 import { UserContextProvider } from "./UserContext";
 
 const Layout = lazyLoad('./Layout/index.jsx');
-const Home = lazyLoad("./pages/Home.jsx");
+const Home = lazyLoad("./pages/Home/index.jsx");
 const NoPage = lazyLoad("./pages/NoPage.jsx");
-const Login = lazyLoad("./pages/Login.jsx");
-const Signup = lazyLoad("./pages/Signup.jsx");
-const Poll = lazyLoad("./pages/Poll.jsx");
-const Dashoard = lazyLoad("./pages/Poll/Dashboard.jsx");
-const CreatePoll = lazyLoad("./pages/Poll/CreatePoll.jsx");
-const EditPoll = lazyLoad('./pages/Poll/EditPoll.jsx');
-const DeletePoll = lazyLoad('./pages/Poll/DeletePoll.jsx');
-const MyPolls = lazyLoad("./pages/Poll/MyPolls.jsx");
-const MyVotes = lazyLoad("./pages/Poll/MyVotes.jsx");
-const MyFeeds = lazyLoad('./pages/MyFeeds.jsx');
-const MyProfile = lazyLoad('./pages/MyProfile.jsx');
-const IsAuthenticatedUser = lazyLoad('./components/IsAuthenticatedUser.jsx');
+const Login = lazyLoad("./pages/Login/index.jsx");
+const Signup = lazyLoad("./pages/Signup/index.jsx");
+const Poll = lazyLoad("./pages/Poll/index.jsx");
+const Dashoard = lazyLoad("./pages/Poll/Dashboard/index.jsx");
+const CreatePoll = lazyLoad("./pages/Poll/CreatePoll/index.jsx");
+const EditPoll = lazyLoad('./pages/Poll/EditPoll/index.jsx');
+const DeletePoll = lazyLoad('./pages/Poll/DeletePoll/index.jsx');
+const MyPolls = lazyLoad("./pages/Poll/MyPolls/index.jsx");
+const MyVotes = lazyLoad("./pages/Poll/MyVotes/index.jsx");
+const MyFeeds = lazyLoad('./pages/MyFeeds/index.jsx');
+const MyProfile = lazyLoad('./pages/MyProfile/index.jsx');
+const IsAuthenticatedUser = lazyLoad('./components/IsAuthenticatedUser/index.jsx');
 
 
 export { NoPage, Layout, Home, Login, Signup, Poll, Dashoard, CreatePoll, EditPoll, DeletePoll, MyPolls, MyVotes, MyFeeds, MyProfile, UserContextProvider, IsAuthenticatedUser }
